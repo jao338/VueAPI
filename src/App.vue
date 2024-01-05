8ac89d71ad03ee680b6b290ab6f67775b41fb3cf
João Henrique <santos.joao@crmsolucoes.com.br>
2024-01-05 15:29:01 -0300

    Ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,19 @@
 <template>
 
-  <div id="app" class="container pt-5">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app" class="container">
 
-    <div class="card mb-3" v-for="user in users" :key="user.name">
-      <div class="card-body">
-        {{ user.name }}
-      </div>
-    </div>
+    <router-view/> 
+
   </div>
-  
+
+
 </template>
 
 <script>
 
-
 export default {
-  name: 'App',
-  components: {
-    
-  },
-
-  data(){
-    return {
-      users: [],
-    }
-  },
-
-  mounted(){
-    fetch('http://127.0.0.1:8000/api/users')
-      .then(response => response.json())
-      .then((res) => {
-          this.users = res.data
-      });
-  }
+  
 }
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div class="card" v-for="user in users" :key="user.id">
+    <div class="card-body">
+      <routerLink :to="{name: 'user-todo', params: {id:user.id}}">{{ user.name }}</routerLink>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  mounted() {
+    fetch('http://127.0.0.1:8000/api/users')
+    .then(response => response.json())
+    .then((res) => {
+      this.users = res.data;
+    });
+  }
+}
+</script>
